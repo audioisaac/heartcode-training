9404bfe208daf3e2ee5f680b437406660e7c2c2c
audioisaac <isaac.ang.2023@scis.smu.edu.sg>
2024-10-11 16:16:56 +0800

    Update page.tsx

--- my-app/app/about-me/page.tsx
+++ my-app/app/about-me/page.tsx
@@ -107,95 +107,95 @@ const Circle = forwardRef<
  
 Circle.displayName = "Circle";
  
-export function AnimatedBeamDemo() {
-  const containerRef = useRef<HTMLDivElement>(null);
-  const div1Ref = useRef<HTMLDivElement>(null);
-  const div2Ref = useRef<HTMLDivElement>(null);
-  const div3Ref = useRef<HTMLDivElement>(null);
-  const div4Ref = useRef<HTMLDivElement>(null);
-  const div5Ref = useRef<HTMLDivElement>(null);
-  const div6Ref = useRef<HTMLDivElement>(null);
-  const div7Ref = useRef<HTMLDivElement>(null);
+// export function AnimatedBeamDemo() {
+//   const containerRef = useRef<HTMLDivElement>(null);
+//   const div1Ref = useRef<HTMLDivElement>(null);
+//   const div2Ref = useRef<HTMLDivElement>(null);
+//   const div3Ref = useRef<HTMLDivElement>(null);
+//   const div4Ref = useRef<HTMLDivElement>(null);
+//   const div5Ref = useRef<HTMLDivElement>(null);
+//   const div6Ref = useRef<HTMLDivElement>(null);
+//   const div7Ref = useRef<HTMLDivElement>(null);
  
-  return (
-    <div
-      className="relative flex h-[500px] w-full items-center justify-center overflow-hidden rounded-lg border bg-background p-10 md:shadow-xl"
-      ref={containerRef}
-    >
-      <div className="flex size-full flex-col max-w-lg max-h-[200px] items-stretch justify-between gap-10">
-        <div className="flex flex-row items-center justify-between">
-          <Circle ref={div1Ref}>
-            <Icons.googleDrive />
-          </Circle>
-          <Circle ref={div5Ref}>
-            <Icons.googleDocs />
-          </Circle>
-        </div>
-        <div className="flex flex-row items-center justify-between">
-          <Circle ref={div2Ref}>
-            <Icons.notion />
-          </Circle>
-          <Circle ref={div4Ref} className="size-16">
-            <Icons.openai />
-          </Circle>
-          <Circle ref={div6Ref}>
-            <Icons.zapier />
-          </Circle>
-        </div>
-        <div className="flex flex-row items-center justify-between">
-          <Circle ref={div3Ref}>
-            <Icons.whatsapp />
-          </Circle>
-          <Circle ref={div7Ref}>
-            <Icons.messenger />
-          </Circle>
-        </div>
-      </div>
+//   return (
+//     <div
+//       className="relative flex h-[500px] w-full items-center justify-center overflow-hidden rounded-lg border bg-background p-10 md:shadow-xl"
+//       ref={containerRef}
+//     >
+//       <div className="flex size-full flex-col max-w-lg max-h-[200px] items-stretch justify-between gap-10">
+//         <div className="flex flex-row items-center justify-between">
+//           <Circle ref={div1Ref}>
+//             <Icons.googleDrive />
+//           </Circle>
+//           <Circle ref={div5Ref}>
+//             <Icons.googleDocs />
+//           </Circle>
+//         </div>
+//         <div className="flex flex-row items-center justify-between">
+//           <Circle ref={div2Ref}>
+//             <Icons.notion />
+//           </Circle>
+//           <Circle ref={div4Ref} className="size-16">
+//             <Icons.openai />
+//           </Circle>
+//           <Circle ref={div6Ref}>
+//             <Icons.zapier />
+//           </Circle>
+//         </div>
+//         <div className="flex flex-row items-center justify-between">
+//           <Circle ref={div3Ref}>
+//             <Icons.whatsapp />
+//           </Circle>
+//           <Circle ref={div7Ref}>
+//             <Icons.messenger />
+//           </Circle>
+//         </div>
+//       </div>
  
-      <AnimatedBeam
-        containerRef={containerRef}
-        fromRef={div1Ref}
-        toRef={div4Ref}
-        curvature={-75}
-        endYOffset={-10}
-      />
-      <AnimatedBeam
-        containerRef={containerRef}
-        fromRef={div2Ref}
-        toRef={div4Ref}
-      />
-      <AnimatedBeam
-        containerRef={containerRef}
-        fromRef={div3Ref}
-        toRef={div4Ref}
-        curvature={75}
-        endYOffset={10}
-      />
-      <AnimatedBeam
-        containerRef={containerRef}
-        fromRef={div5Ref}
-        toRef={div4Ref}
-        curvature={-75}
-        endYOffset={-10}
-        reverse
-      />
-      <AnimatedBeam
-        containerRef={containerRef}
-        fromRef={div6Ref}
-        toRef={div4Ref}
-        reverse
-      />
-      <AnimatedBeam
-        containerRef={containerRef}
-        fromRef={div7Ref}
-        toRef={div4Ref}
-        curvature={75}
-        endYOffset={10}
-        reverse
-      />
-    </div>
-  );
-}
+//       <AnimatedBeam
+//         containerRef={containerRef}
+//         fromRef={div1Ref}
+//         toRef={div4Ref}
+//         curvature={-75}
+//         endYOffset={-10}
+//       />
+//       <AnimatedBeam
+//         containerRef={containerRef}
+//         fromRef={div2Ref}
+//         toRef={div4Ref}
+//       />
+//       <AnimatedBeam
+//         containerRef={containerRef}
+//         fromRef={div3Ref}
+//         toRef={div4Ref}
+//         curvature={75}
+//         endYOffset={10}
+//       />
+//       <AnimatedBeam
+//         containerRef={containerRef}
+//         fromRef={div5Ref}
+//         toRef={div4Ref}
+//         curvature={-75}
+//         endYOffset={-10}
+//         reverse
+//       />
+//       <AnimatedBeam
+//         containerRef={containerRef}
+//         fromRef={div6Ref}
+//         toRef={div4Ref}
+//         reverse
+//       />
+//       <AnimatedBeam
+//         containerRef={containerRef}
+//         fromRef={div7Ref}
+//         toRef={div4Ref}
+//         curvature={75}
+//         endYOffset={10}
+//         reverse
+//       />
+//     </div>
+//   );
+// }
  
 const Icons = {
   notion: () => (
@@ -655,7 +655,7 @@ const AboutMe: NextPage = () => {
           <CardContent className="flex flex-row">
             <div className="flex flex-row">
             </div>
-            <AnimatedBeamDemo />
+            {/* <AnimatedBeamDemo /> */}
           </CardContent>
           {/* <Image alt="funny picture" src={darkseer}/> */}
         </Card>
